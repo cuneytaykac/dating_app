7077e3a467fbd29a929ea79356beab1b6b8a3acf
Cuneyt AYKAC <cuneyt.aykac@duzey.com.tr>
2025-08-03 15:19:34 +0300

    Update Android and iOS configurations: Upgrade Kotlin version to 2.1.10, set minSdk to 23, and change app label to "SIN FLIX". Replace app icon with new logo across all resolutions and update Info.plist for consistent branding. Add new app icon images to asset catalog.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,6 +14,7 @@ android {
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -28,7 +29,7 @@ android {
         applicationId = "com.example.dating_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
